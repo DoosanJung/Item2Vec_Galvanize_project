73e8584adb70ef8a84c2a3fb23f654a8fd10e882
DoosanJung <dootaa00@gmail.com>
2018-02-20 23:32:24 -0800

    add logging module to TestInstallation

--- I2V_project/util/TestInstallation.py
+++ I2V_project/util/TestInstallation.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
+'''
+'''
 import unittest
+import logging
+from logging import config
 
 class ModuleImportTestCase(unittest.TestCase):
 	def test_gensim(self):
 		result = False
-		print "try to test gensim installation..."
-		try:	
+		logger.info("try to test gensim installation...")
+		try:
 			import gensim
-			print "succeed in importing gensim" 		
+			logger.info("succeed in importing gensim")
 		except:
-			print "Failed to import gensim"
-			raise	
+			logger.error("Failed to import gensim")
+			raise
 
 if __name__=='__main__':
 	unittest.main()
-
